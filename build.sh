3c5df99bd6991012d7bdb90d0b7c8d6b0eeb6e63
newbee1905 <beenewminh@outlook.com>
2024-11-14 03:32:44 +1100

    chore: first commit - setting up fmt

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CC=g++
+
+INCLUDES="libs/fmt/include include"
+LIBS="libs/fmt/build"
+LIBS_TO_LINK="fmt"
+
+BINARY="ranobe"
+
+INCLUDE_FLAGS=""
+LIBRARY_FLAGS=""
+LINK_FLAGS=""
+
+for inc in $INCLUDES; do
+	INCLUDE_FLAGS="$INCLUDE_FLAGS -I$inc"
+done
+
+for lib in $LIBS; do
+	LIBRARY_FLAGS="$LIBRARY_FLAGS -L$lib"
+done
+
+for lib in $LIBS_TO_LINK; do
+	LINK_FLAGS="$LINK_FLAGS -l$lib"
+done
+
+echo "$CC $INCLUDE_FLAGS -o $BINARY src/*.cc $LIBRARY_FLAGS $LINK_FLAGS $@"
+$CC $INCLUDE_FLAGS -o $BINARY src/*.cc $LIBRARY_FLAGS $LINK_FLAGS $@
+
+if [ $? -eq 0 ]; then
+	echo "Build successful. Output file: $BINARY"
+else
+	echo "Build failed."
+fi
+
